58860fa54c69a0f10b8d505c52907317462bfd7c
hadesyi <hadesleeq@gmail.com>
2016-08-05 17:57:16 +0900

    post new entity

---
+++ src/main/java/org/ksug/board/Post.java
@@ -0,0 +1,34 @@
+package org.ksug.board;
+
+import javax.persistence.Entity;
+import java.util.Date;
+
+/**
+ * Project: SpringBootHandsonlan
+ * FileName: Post
+ * Date: 2016-08-05
+ * Time: 오후 5:53
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+public class Post {
+    private long id;
+    private String author;
+    private String title;
+    private String content;
+    private Date createdAt;
+    private Board board;
+
+    public Post() {
+    }
+
+    public Post(String author, String title, String content, Board board) {
+        this.author = author;
+        this.title = title;
+        this.content = content;
+        this.board = board;
+        this.createdAt = new Date();
+    }
+}

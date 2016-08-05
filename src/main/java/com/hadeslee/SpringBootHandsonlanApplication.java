5f44713e86ae7c4e65838ba6084210acd6183b50
hadesyi <hadesleeq@gmail.com>
2016-08-05 17:52:52 +0900

    Initial commit

---
+++ src/main/java/com/hadeslee/SpringBootHandsonlanApplication.java
@@ -0,0 +1,12 @@
+package com.hadeslee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHandsonlanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHandsonlanApplication.class, args);
+	}
+}
---
+++ src/main/java/org/ksug/board/Board.java
@@ -0,0 +1,43 @@
+package org.ksug.board;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
+/**
+ * Project: SpringBootHandsonlan
+ * FileName: Board
+ * Date: 2016-08-05
+ * Time: 오후 5:47
+ * Author: Hadeslee
+ * Note:
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+public class Board {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String name;
+    private Date createdAt;
+
+    protected Board() {
+    }
+
+    public Board(String name) {
+        this.name = name;
+        this.createdAt = new Date();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+}
+
+

ce9f4cdacb8b4d44850a5bf58bb733aaa2b23161
hadesyi <hadesleeq@gmail.com>
2016-08-08 09:43:07 +0900

    Board 서비스 생성

--- src/main/java/org/ksug/board/module/BoardService.java
+++ src/main/java/org/ksug/board/module/BoardService.java
@@ -1,8 +1,12 @@
 package org.ksug.board.module;
 
+import org.ksug.board.Board;
+import org.ksug.board.Post;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 /**
  * Project: SpringBootHandsonlan
@@ -18,4 +22,30 @@ import javax.transaction.Transactional;
 public class BoardService {
     private BoardRepository boardRepository;
     private PostRepository postRepository;
+
+    @Autowired
+    public void setBoardRepository(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    @Autowired
+    public void setPostRepository(PostRepository postRepository) {
+        this.postRepository = postRepository;
+    }
+
+    public Board findBoard(String boardname) {
+        Board board = boardRepository.findByName(boardname);
+        return board;
+    }
+
+    public List<Post> findPosts(String boardname) {
+        Board board = findBoard(boardname);
+        return postRepository.findByBoard(board);
+    }
+
+    private Post getPost(long postId) {
+        Post post = postRepository.findOne(postId);
+        return post;
+    }
+
 }

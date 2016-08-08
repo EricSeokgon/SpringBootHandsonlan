475c12178b28413a89c13fbeaefd9a654d1315f1
hadesyi <hadesleeq@gmail.com>
2016-08-08 10:10:30 +0900

    컨트롤러 생성

--- src/main/java/org/ksug/webservice/BoardController.java
+++ src/main/java/org/ksug/webservice/BoardController.java
@@ -1,9 +1,16 @@
 package org.ksug.webservice;
 
+import org.ksug.board.Board;
+import org.ksug.board.Post;
 import org.ksug.board.module.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * Project: SpringBootHandsonlan
@@ -22,5 +29,15 @@ public class BoardController {
     @Autowired
     public void setBoardService(BoardService boardService) {
         this.boardService = boardService;
+    }
+
+    @RequestMapping(value = "/{boardname}/info", method = {RequestMethod.GET, RequestMethod.HEAD})
+    public ResponseEntity<Board> info(@PathVariable String boardname) {
+        return ResponseEntity.ok(boardService.findBoard(boardname));
+    }
+
+    @RequestMapping(value = "{boardname", method = {RequestMethod.GET, RequestMethod.HEAD})
+    public ResponseEntity<List<Post>> listPosts(@PathVariable String boardname) {
+        return ResponseEntity.ok(boardService.findPosts(boardname));
     }
 }
